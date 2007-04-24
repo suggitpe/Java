3405231e0e703178aefde28d837d9b208f80bfc2
suggitpe <suggitpe@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2007-04-24 18:22:01 +0000

    Updated tests and started on the schema creator

---
+++ sandbox/hibernate/src/test/java/com/suggs/sandbox/hibernate/schemaCreator/SchemaCreatorTest.java
@@ -0,0 +1,15 @@
+/*
+ * SchemaCreatorTest.java created on 24 Apr 2007 18:21:26 by suggitpe for project SandBox - Hibernate
+ * 
+ */
+package com.suggs.sandbox.hibernate.schemaCreator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class SchemaCreatorTest
+{
+
+    private static final Log LOG = LogFactory.getLog( SchemaCreatorTest.class );
+}
