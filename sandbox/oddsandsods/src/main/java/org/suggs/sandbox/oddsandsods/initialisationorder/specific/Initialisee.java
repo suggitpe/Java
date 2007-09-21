d98f6b3dd3a7525eb95d202c530f8885c9bf62ef
suggitpe <suggitpe@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2007-09-21 20:29:32 +0000

    sort issue

---
+++ sandbox/oddsandsods/src/main/java/org/suggs/sandbox/oddsandsods/initialisationorder/specific/Initialisee.java
@@ -0,0 +1,67 @@
+/*
+ * Initialisee.java created on 21 Sep 2007 06:30:30 by suggitpe for project SandBox - OddsAndSods
+ * 
+ */
+package org.suggs.sandbox.oddsandsods.initialisationorder.detailed;
+
+/**
+ * A class that can be initialised in different ways
+ * 
+ * @author suggitpe
+ * @version 1.0 21 Sep 2007
+ */
+public class Initialisee
+{
+
+    public static Initialisee instance = new Initialisee( "Static member" );
+
+    /**
+     * Singleton approach to construction
+     * 
+     * @return an initialisee
+     */
+    public static Initialisee instance()
+    {
+        return new Initialisee( "Static method" );
+    }
+
+    /**
+     * Constructs a new instance.
+     */
+    public Initialisee()
+    {
+        Logger.log( "Default constructor" );
+    }
+
+    protected String mMember_;
+
+    /**
+     * Constructs a new instance.
+     * 
+     * @param aType
+     *            type of construction
+     */
+    public Initialisee( String aType )
+    {
+        Logger.log( aType + " Constructor" );
+    }
+
+    static
+    {
+        Logger.log( "Static initialiser" );
+    }
+
+    {
+        Logger.log( "Non-static initialiser" );
+    }
+
+    static
+    {
+        Logger.log( "Static initialiser 2" );
+    }
+
+    {
+        Logger.log( "Non-static initialiser 2" );
+    }
+
+}
---
+++ sandbox/oddsandsods/src/main/java/org/suggs/sandbox/oddsandsods/initialisationorder/specific/Logger.java
@@ -0,0 +1,51 @@
+/*
+ * Logger.java created on 20 Sep 2007 18:18:06 by suggitpe for project SandBox - OddsAndSods
+ * 
+ */
+package org.suggs.sandbox.oddsandsods.initialisationorder.detailed;
+
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Static class to catch the output of the tests in the correct order
+ * 
+ * @author suggitpe
+ * @version 1.0 20 Sep 2007
+ */
+public class Logger
+{
+
+    private static final List<String> LOGS = new Vector<String>( 10 );
+
+    /**
+     * Adds a message to the internal vector
+     * 
+     * @param aMsg
+     *            the message to add
+     */
+    public static synchronized void log( String aMsg )
+    {
+        LOGS.add( aMsg );
+    }
+
+    /**
+     * Outputs the contents of the internal vector to the console
+     */
+    public static synchronized void output()
+    {
+        for ( String s : LOGS )
+        {
+            System.out.println( s );
+        }
+    }
+
+    /**
+     * Empty all messages from the logs
+     */
+    public static synchronized void clearLogs()
+    {
+        LOGS.clear();
+    }
+
+}
