5633e6b66ebeb8b7bddec0b9713fada5d0b3447a
(no author) <(no author)@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2009-07-16 11:06:23 +0000

    IN PROGRESS - issue SANDBOX-13: Build bundle release tool GUI that works as a bundle in Felix http://jirahost:9090/jira/browse/SANDBOX-13

---
+++ sandbox/gui/src/main/java/org/suggs/sandbox/table/ClipboardHelper.java
@@ -0,0 +1,91 @@
+/*
+ * ClipboardHelper.java created on 15 Jul 2009 18:50:31 by suggitpe for project SandBox - GUI
+ * 
+ */
+package org.suggs.sandbox.table;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * Class to house a collection of clipboard manipulation methods -
+ * used in the Table sandlbox
+ * 
+ * @author suggitpe
+ * @version 1.0 15 Jul 2009
+ */
+public class ClipboardHelper
+{
+
+    /**
+     * Gets the contents of the clipboard and returns it as a string
+     * 
+     * @param requestor
+     * @return
+     */
+    static String getClipboardContents( Object requestor )
+    {
+        Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard().getContents( requestor );
+        if ( t != null )
+        {
+            DataFlavor df = DataFlavor.stringFlavor;
+            if ( df != null )
+            {
+                try
+                {
+                    Reader r = df.getReaderForText( t );
+                    char[] charBuf = new char[512];
+                    StringBuffer buf = new StringBuffer();
+                    int n;
+                    while ( ( n = r.read( charBuf, 0, charBuf.length ) ) > 0 )
+                    {
+                        buf.append( charBuf, 0, n );
+                    }
+                    r.close();
+                    return ( buf.toString() );
+                }
+                catch ( IOException ex )
+                {
+                    ex.printStackTrace();
+                }
+                catch ( UnsupportedFlavorException ex )
+                {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Checks the contents of the clipboard to see if it contains data
+     * and that the data is textual. and that the
+     * 
+     * @param requestor
+     * @return true if the data is a string and that the content is
+     *         plain text, else false
+     */
+    static boolean isClipboardContainingText( Object requestor )
+    {
+        Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard().getContents( requestor );
+        return t != null
+               && ( t.isDataFlavorSupported( DataFlavor.stringFlavor ) || t.isDataFlavorSupported( DataFlavor.getTextPlainUnicodeFlavor() ) );
+    }
+
+    /**
+     * Sets the contents of the clipboard to the passed in string
+     * 
+     * @param s
+     *            the string to drop into the clipboard
+     */
+    static void setClipboardContents( String s )
+    {
+        StringSelection selection = new StringSelection( s );
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents( selection, selection );
+    }
+}
