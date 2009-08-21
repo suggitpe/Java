c677215035240509bfeddbd02e9e8700545dc5e7
(no author) <(no author)@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2009-08-21 16:23:50 +0000

    Updated SVN properties, added new libaries component

--- sandbox/hibernate/src/test/java/org/suggs/sandbox/hibernate/AllTests.java
+++ sandbox/hibernate/src/test/java/org/suggs/sandbox/hibernate/AllTests.java
@@ -4,8 +4,15 @@
  */
 package org.suggs.sandbox.hibernate;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.suggs.sandbox.hibernate.caveatEmptor.CaveatEmptorAnnotationObjectTest;
+import org.suggs.sandbox.hibernate.caveatEmptor.CaveatEmptorAnnotationRelationshipTest;
+import org.suggs.sandbox.hibernate.caveatEmptor.CaveatEmptorHbmObjectTest;
+import org.suggs.sandbox.hibernate.caveatEmptor.CaveatEmptorHbmRelationshipTest;
+import org.suggs.sandbox.hibernate.chapter2.MessageHibernateAnnotationDaoTest;
+import org.suggs.sandbox.hibernate.chapter2.MessageHibernateHbmDaoTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 /**
  * Test suite for Hibernate sandbox
@@ -13,21 +20,10 @@ import junit.framework.TestSuite;
  * @author suggitpe
  * @version 1.0 13 Apr 2007
  */
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { MessageHibernateHbmDaoTest.class, MessageHibernateAnnotationDaoTest.class,
+                      CaveatEmptorHbmObjectTest.class, CaveatEmptorHbmRelationshipTest.class,
+                      CaveatEmptorAnnotationObjectTest.class,
+                      CaveatEmptorAnnotationRelationshipTest.class })
 public class AllTests
-{
-
-    /**
-     * @return test suite
-     */
-    public static Test suite()
-    {
-        TestSuite s = new TestSuite();
-        s.addTestSuite( org.suggs.sandbox.hibernate.chapter2.MessageHibernateHbmDaoTest.class );
-        s.addTestSuite( org.suggs.sandbox.hibernate.chapter2.MessageHibernateAnnotationDaoTest.class );
-        s.addTestSuite( org.suggs.sandbox.hibernate.caveatEmptor.CaveatEmptorHbmObjectTest.class );
-        s.addTestSuite( org.suggs.sandbox.hibernate.caveatEmptor.CaveatEmptorHbmRelationshipTest.class );
-        s.addTestSuite( org.suggs.sandbox.hibernate.caveatEmptor.CaveatEmptorAnnotationObjectTest.class );
-        s.addTestSuite( org.suggs.sandbox.hibernate.caveatEmptor.CaveatEmptorAnnotationRelationshipTest.class );
-        return s;
-    }
-}
+{}
