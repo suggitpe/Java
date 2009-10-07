c66040d2df87d660faeb894bd5896b7f48c78401
(no author) <(no author)@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2009-10-07 17:29:28 +0000

    Added a crazy arsed exception test to oddsandsods

---
+++ sandbox/oddsandsods/src/main/java/org/suggs/sandbox/oddsandsods/exceptions/ExceptionTest.java
@@ -0,0 +1,55 @@
+/*
+ * ExceptionTests.java created on 7 Oct 2009 18:22:12 by suggitpe for project SandBox - OddsAndSods
+ * 
+ */
+package org.suggs.sandbox.oddsandsods.exceptions;
+
+import org.suggs.sandbox.oddsandsods.logger.Log;
+import org.suggs.sandbox.oddsandsods.logger.LogFactory;
+
+/**
+ * TODO Write javadoc for ExceptionTests
+ * 
+ * @author suggitpe
+ * @version 1.0 7 Oct 2009
+ */
+public class ExceptionTest
+{
+
+    private static final Log LOG = LogFactory.getLog( ExceptionTest.class );
+
+    public static void main( String[] args )
+    {
+        try
+        {
+            testme();
+        }
+        catch ( Exception e )
+        {
+            // whatever!!!
+            LOG.error( "This is from main", e );
+        }
+    }
+
+    public static void testme() throws Exception
+    {
+        try
+        {
+            LOG.debug( "In try" );
+            throw new Exception( "This is the main exception in the try" );
+        }
+        catch ( Exception e )
+        {
+            LOG.debug( "In catch" );
+            throw new Exception( "this is in the catch" );
+        }
+        finally
+        {
+            LOG.debug( "In finally)" );
+            if ( 1 == 1 )
+            {
+                throw new Exception( "this is in finally" );
+            }
+        }
+    }
+}
