76e366a6c67b710ee9818d8161f5c125f6036952
(no author) <(no author)@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2010-02-22 20:12:28 +0000

    Created basic unit tests for the JAXB message builder

---
+++ sandbox/jaxb/testclient/src/main/java/org/suggs/sandbox/jaxb/client/MessageBuilder.java
@@ -0,0 +1,25 @@
+/*
+ * MessageBuilder.java created on 17 Feb 2010 07:14:50 by suggitpe for project sandbox-jaxb-test-client
+ * 
+ */
+package org.suggs.sandbox.jaxb.client;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.ubs.firc.scribe.message.supplementary.SupplementaryData;
+
+/**
+ * TODO Write javadoc for MessageBuilder
+ * 
+ * @author suggitpe
+ * @version 1.0 17 Feb 2010
+ */
+public class MessageBuilder
+{
+
+    private static final Log LOG = LogFactory.getLog( MessageBuilder.class );
+
+    SupplementaryData data;
+
+}
---
+++ sandbox/jaxb/testclient/src/test/java/org/suggs/sandbox/jaxb/client/MessageBuilderTest.java
@@ -0,0 +1,63 @@
+/*
+ * MessageBuilderTest.java created on 17 Feb 2010 07:15:47 by suggitpe for project sandbox-jaxb-test-client
+ * 
+ */
+package org.suggs.sandbox.jaxb.client;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+/**
+ * TODO Write javadoc for MessageBuilderTest
+ * 
+ * @author suggitpe
+ * @version 1.0 17 Feb 2010
+ */
+public class MessageBuilderTest
+{
+
+    private static final Log LOG = LogFactory.getLog( MessageBuilderTest.class );
+
+    @Test
+    public void createBasicXmlMessage()
+    {
+        LOG.debug( "Creating test XML file" );
+    }
+
+    @Test
+    public void validateGoodXmlMessage()
+    {
+        LOG.debug( "Validating good XML message" );
+    }
+
+    @Test
+    public void validateBadXmlMessage()
+    {
+        LOG.debug( "Validating bad XML message" );
+    }
+
+    @Test
+    public void serialiseValidXmlMessage()
+    {
+        LOG.debug( "Serialising valid XML message" );
+    }
+
+    @Test
+    public void serialiseInvalidXmlMessage()
+    {
+        LOG.debug( "Serialising invalid XML message" );
+    }
+
+    @Test
+    public void readGoodXmlFileIntoMemory()
+    {
+        LOG.debug( "Reading XML file from disk" );
+    }
+
+    @Test
+    public void readBadXmlFileIntoMemory()
+    {
+        LOG.debug( "Reading XML file from disk" );
+    }
+}
