0c624b9d48233f1e6eb7c36d0819516d610ba15d
(no author) <(no author)@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2010-05-18 09:52:20 +0000

    removed PMD warning for equals method.

--- sandbox/sandbox-core/hibernate/src/main/java/org/suggs/sandbox/hibernate/timestamps/TimestampedEntity.java
+++ sandbox/sandbox-core/hibernate/src/main/java/org/suggs/sandbox/hibernate/timestamps/TimestampedEntity.java
@@ -177,6 +177,7 @@ public class TimestampedEntity extends EntityBase {
      * @see java.lang.Object#equals(java.lang.Object)
      */
     @Override
+    @SuppressWarnings("PMD.IfStmtsMustUseBraces")
     public boolean equals( Object obj ) {
         if ( this == obj )
             return true;
