61e11d245aa2d3f2462c900eb59c24596966e742
(no author) <(no author)@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2010-10-04 18:08:42 +0000

    Added Hibernate Interceptor into the jms persistor

--- standalone-projects/jms-to-db-persister/src/main/java/com/ubs/reporting/domain/support/StagedAuditInfo.java
+++ standalone-projects/jms-to-db-persister/src/main/java/com/ubs/reporting/domain/support/StagedAuditInfo.java
@@ -4,29 +4,89 @@
  */
 package com.ubs.reporting.domain.support;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * TODO Write javadoc for StagedAuditInfo
+ * Auditable information used in conjunction with a Staged Entity.
  * 
  * @author suggitpe
  * @version 1.0 4 Oct 2010
  */
+@Embeddable
 public class StagedAuditInfo {
 
     @SuppressWarnings("unused")
     private static final Log LOG = LogFactory.getLog( StagedAuditInfo.class );
 
-    // @Column(name = "STG_EFFECTIVE_DATE")
-    // private Date stagingEffectiveDate;
-    //
-    // @Column(name = "STG_LOAD_DATE")
-    // private Date stagingLoadDate;
-    //
-    // @Column(name = "STG_LOAD_ID")
-    // private Integer stagingLoadId;
-    //
-    // @Column(name = "STG_JOB_RUN_ID")
-    // private Integer stagingJobRunId;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "STG_LOAD_DATE")
+    private Date stagingLoadDate;
+
+    /**
+     * Returns the value of stagingLoadDate.
+     * 
+     * @return Returns the stagingLoadDate.
+     */
+    public Date getStagingLoadDate() {
+        return stagingLoadDate;
+    }
+
+    /**
+     * Sets the stagingLoadDate field to the specified value.
+     * 
+     * @param aStagingLoadDate
+     *            The stagingLoadDate to set.
+     */
+    public void setStagingLoadDate( Date aStagingLoadDate ) {
+        stagingLoadDate = aStagingLoadDate;
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( stagingLoadDate == null ) ? 0 : stagingLoadDate.hashCode() );
+        return result;
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals( Object obj ) {
+        if ( this == obj )
+            return true;
+        if ( obj == null )
+            return false;
+        if ( getClass() != obj.getClass() )
+            return false;
+        StagedAuditInfo other = (StagedAuditInfo) obj;
+        if ( stagingLoadDate == null ) {
+            if ( other.stagingLoadDate != null )
+                return false;
+        }
+        else if ( !stagingLoadDate.equals( other.stagingLoadDate ) )
+            return false;
+        return true;
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "StagedAuditInfo [stagingLoadDate=" + stagingLoadDate + "]";
+    }
+
 }
