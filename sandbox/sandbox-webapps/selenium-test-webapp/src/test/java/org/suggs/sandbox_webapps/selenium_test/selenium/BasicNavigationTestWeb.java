974c252e6605df75843e34122d5b5cdb662e83cd
(no author) <(no author)@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2011-05-06 15:26:37 +0000

    Updated surefire plugin to execute web tests on a tomcat instance based on name.

---
+++ sandbox/sandbox-webapps/selenium-test-webapp/src/test/java/org/suggs/sandbox_webapps/selenium_test/selenium/BasicNavigationTestWeb.java
@@ -0,0 +1,61 @@
+package org.suggs.sandbox_webapps.selenium_test.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * This is a simple selenium test that allows us to demonstrate the use of selenium as a test tool against a web
+ * application.
+ * <p/>
+ * User: suggitpe
+ * Date: 05/05/11
+ * Time: 18:33
+ */
+
+public class BasicNavigationTestWeb {
+
+    @SuppressWarnings("unused")
+    private static final Logger LOG = LoggerFactory.getLogger( BasicNavigationTestWeb.class );
+
+    //private final WebDriver webDriver = new FirefoxDriver();
+    private final WebDriver webDriver = new HtmlUnitDriver();
+
+    private static final String BASE_URL = "http://localhost:9099/selenium-webapp-test";
+    private static final String BASE_URL_TITLE = "Selenium Test WebApp";
+    private static final String FUNKY_URL_TITLE = "Some Funky Page";
+
+    @Before
+    public void onSetup() {
+        webDriver.get( BASE_URL );
+        assertThat( webDriver.getTitle(), equalTo( BASE_URL_TITLE ) );
+    }
+
+    @After
+    public void onTeardown() {
+        webDriver.close();
+    }
+
+    @Test
+    public void opensInitialPageCorrectly() {
+        LOG.debug( "verifying that we have the right page" );
+        assertThat( webDriver.getTitle(), equalTo( BASE_URL_TITLE ) );
+    }
+
+
+    @Test
+    public void navigateToFunkyPage() {
+        LOG.debug( "Navigating to funky page" );
+        webDriver.findElement( By.id( "funkyLink" ) ).click();
+        LOG.debug( "Verifying that we have the right page" );
+        assertThat( webDriver.getTitle(), equalTo( FUNKY_URL_TITLE ) );
+    }
+}
