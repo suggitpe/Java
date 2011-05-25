e016d5318e7518705ccd44aad7be37341df444f4
(no author) <(no author)@facf25d5-02cb-8b44-963b-a2ef5ac01c67>
2011-05-25 06:53:03 +0000

    Updated POM and easyb tests so that they execute

---
+++ sandbox/sandbox-core/test/src/test/easyb/org/suggs/sandbox/easyb/ICanToggleACellStory.groovy
@@ -0,0 +1,25 @@
+package org.suggs.sandbox.test.easyb
+
+import org.suggs.sandbox.test.basicscenario.Game
+
+scenario "With a square grid I can change various points on the grid" {
+
+    def Game game
+
+    given "a 5 by 5 game" {
+        game = Game(5, 5)
+    }
+
+    when "I toggle the cell at (2, 3)" {
+        game.toggleCellAt(2, 3)
+    }
+
+    then "the " {
+        def expected = """
+    .....
+    .....
+    .....
+    ..X..
+    ....."""
+    }
+}
